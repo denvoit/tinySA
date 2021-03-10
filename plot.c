f3808b58f01b06813070248c1f36ea9ef82cc228
DiSlord <dislord@mail.ru>
2021-03-10 22:11:27 +0300

    Made more fast waterfall +~30% from old DMA rx mode, and 3x faster from old no DMA mode

--- plot.c
+++ plot.c
@@ -1632,11 +1632,6 @@ static void update_waterfall(void){
 //  STOP_PROFILE;
 }
 
-int get_waterfall(void)
-{
-  return(setting.waterfall);
-}
-
 //extern float peakLevel;
 //extern float min_level;
 //int w_max = -130;
@@ -1646,23 +1641,20 @@ toggle_waterfall(void)
 {
   if (setting.waterfall == W_SMALL)       graph_bottom = SMALL_WATERFALL;
   else if (setting.waterfall == W_BIG)    graph_bottom = BIG_WATERFALL;
-  else /*if (setting.waterfall = W_OFF)*/ graph_bottom = NO_WATERFALL;
-
+  else /*if (setting.waterfall == W_OFF)*/graph_bottom = NO_WATERFALL;
   _grid_y = graph_bottom / NGRIDY;
-  ili9341_set_background(LCD_BG_COLOR);
-  ili9341_fill(OFFSETX, graph_bottom, LCD_WIDTH - OFFSETX, CHART_BOTTOM - graph_bottom);
+  if (setting.waterfall != W_OFF){
+    ili9341_set_background(LCD_BG_COLOR);
+    ili9341_fill(OFFSETX, graph_bottom, LCD_WIDTH - OFFSETX, CHART_BOTTOM - graph_bottom);
+  }
   request_to_redraw_grid();
 }
 
 void
 disable_waterfall(void)
 {
-  graph_bottom = NO_WATERFALL;
   setting.waterfall = W_OFF;
-  _grid_y = graph_bottom / NGRIDY;
-  ili9341_set_background(LCD_BG_COLOR);
-  ili9341_fill(OFFSETX, graph_bottom, LCD_WIDTH - OFFSETX, CHART_BOTTOM - graph_bottom);
-  request_to_redraw_grid();
+  toggle_waterfall();
 }
 
 void
